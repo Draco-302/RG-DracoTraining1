f2e5822a1eba5e14a3db6ff90672f9ae14c16ee2
Draco-302 <regan.rundi302@gmail.com>
2024-10-08 17:10:25 +0700

    Add files via upload

---
+++ Training 1.py
@@ -0,0 +1,11 @@
+Player_Name = "Gathan"
+Score = 42096
+Time_Left = 10.5
+Game_Over = False
+Enemies = ["Rice", "Hamburger", "Pork"]
+
+print(f"Player : {Player_Name}")
+print(f"Score : {Score}")
+print(f"Time left(s) : {Time_Left}")
+print(f"Dead ? : {Game_Over}")
+print(f"Enemies : {Enemies }")
